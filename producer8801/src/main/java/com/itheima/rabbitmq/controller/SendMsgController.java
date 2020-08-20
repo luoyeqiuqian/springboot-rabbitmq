d8fe0411044aa092222cd34678fec407c7ba0f20
wangjingen <Wjg6677..123>
2020-08-20 18:50:02 +0800

    Initial commit

---
+++ producer8801/src/main/java/com/itheima/rabbitmq/controller/SendMsgController.java
@@ -0,0 +1,35 @@
+package com.itheima.rabbitmq.controller;
+
+import com.itheima.rabbitmq.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SendMsgController {
+
+    //注入RabbitMQ的模板
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    /**
+     * 测试
+     * http://localhost:8801/sendMsg?msg=qqqqqqq&key=item.a
+     */
+    @GetMapping("/sendMsg")
+    public String sendMsg(@RequestParam String msg, @RequestParam String key){
+
+        /**
+         * 发送消息
+         * 参数一：交换机名称
+         * 参数二：路由key
+         * 参数三：发送的消息
+         */
+        rabbitTemplate.convertAndSend(RabbitMQConfig.ITEM_TOPIC_EXCHANGE ,key ,msg);
+
+        //返回消息
+        return "发送消息成功！";
+    }
+}
